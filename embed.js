db3c8527664f7c793493df5cf19efec31c322771
Abubakar Abdul-Wali <abubakarabdulwali7@gmail.com>
2025-04-20 04:53:48 +0100

    Update embed.js

--- embed.js
+++ embed.js
@@ -3,17 +3,23 @@
   container.id = 'chatbot-container';
   document.body.appendChild(container);
 
-  const html = await fetch('https://wali-dt.github.io/chatbot/widget.html').then(res => res.text());
+  // Fetch resources
+  const html = await fetch('https://wali-dt.github.io/chatbot/index.html').then(res => res.text());
   const style = await fetch('https://wali-dt.github.io/chatbot/style.css').then(res => res.text());
   const script = await fetch('https://wali-dt.github.io/chatbot/script.js').then(res => res.text());
 
-  container.innerHTML = html;
+  // Extract body content only
+  const bodyMatch = html.match(/<body[^>]*>([\s\S]*)<\/body>/i);
+  container.innerHTML = bodyMatch ? bodyMatch[1] : html;
 
+  // Inject style
   const styleTag = document.createElement('style');
   styleTag.textContent = style;
   document.head.appendChild(styleTag);
 
+  // Inject script
   const scriptTag = document.createElement('script');
   scriptTag.textContent = script;
   document.body.appendChild(scriptTag);
 })();
+
