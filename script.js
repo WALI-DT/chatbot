00c1c51515e1ab14fa5d8ec10b262daed69c9581
Abubakar Abdul-Wali <abubakarabdulwali7@gmail.com>
2025-04-20 05:43:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ async function sendMessage() {
   chatMessages.scrollTop = chatMessages.scrollHeight;
 
   try {
-    const res = await fetch("https://abdul-wali.app.n8n.cloud/webhook/c0423075-9067-4e31-b2a1-c32c0e4a3ac8", {
+    const res = await fetch("https://abdul-wali.app.n8n.cloud/webhook-test/c0423075-9067-4e31-b2a1-c32c0e4a3ac8", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ message: text }),
